7d55d49be7b888691fbe7beb24b68b57d45460a8
andebhadra2 <andebhadra2@gmail.com>
2019-03-20 00:31:16 +0530

    adding solr java classes

--- SolrSpringBoot/src/main/java/com/solr/app/SolrSpringBoot/SolrSpringBootApplication.java
+++ SolrSpringBoot/src/main/java/com/solr/app/SolrSpringBoot/SolrSpringBootApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages= {
-		"com.doj.app.config","com.doj.app.controller","com.doj.app.pojo","com.doj.app","com.doj.app.repository"
+		"com.solr.app.*"
 		})
 public class SolrSpringBootApplication {
 
